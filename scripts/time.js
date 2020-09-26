a952ac1c77bced96e5f609aaf9e81188d88385c7
Sandy FedoraX <ctu@ku.edu>
2020-09-26 02:16:51 -0500

    Added time to the details div
    Signed-off-by: Sandy FedoraX <ctu@ku.edu>

---
+++ scripts/time.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let time = document.querySelector("#time");
+  const day = 24 * 60 * 60 * 1000;
+
+  setTime = () => {
+    let now = new Date();
+    let start = new Date(now.getFullYear(), 0, 0);
+    var diff =
+      now -
+      start +
+      (start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000;
+
+    time.innerText =
+      (now.getHours() > 0 && now.getHours() < 12 ? "🌑" : "🌕") +
+      " " +
+      Math.floor(diff / day) +
+      "; " +
+      (now.getFullYear() + 10000) +
+      " H.E. " +
+      (now.getHours() * 100 + now.getMinutes()).toString().padStart(4, "0") +
+      "." +
+      now.getSeconds().toString().padStart(2, "0");
+  };
+
+  setTime();
+  setInterval(setTime, 1000);
+});
