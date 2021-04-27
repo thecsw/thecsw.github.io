162261d2c9ac95cc6a6331da20d3aaa5cae1a236
Sandy Urazayev <ctu@ku.edu>
2021-04-27 18:07:02 -0500

    Ignore timestamps as descriptions
    Signed-off-by: Sandy Urazayev <ctu@ku.edu>

--- sh/og_desc.sh
+++ sh/og_desc.sh
@@ -8,8 +8,8 @@ then
     sedcom="gsed -E"
 fi
 
-toinsert=`cat $1 | grep '<p>' | $sedcom "/H[.]E[.]/d;/2020\s*$/d" | $sedcom -n 1p | $sedcom 's/<p>//' | $sedcom 's/<[^<>]+>//g' | $sedcom 's|^|/<title/i\\\\<meta property="og:description" content="|' | $sedcom 's|$|">|'`
+toinsert=`cat $1 | grep '<p>' | $sedcom '/H[.]E[.]/d;/20(19|20)\s*<[^<>]+>\s*$/d' | $sedcom -n 1p | $sedcom 's/<p>//' | $sedcom 's/<[^<>]+>//g' | $sedcom 's|^|/<title/i\\\\<meta property="og:description" content="|' | $sedcom 's|$|">|'`
 $sedcom "$toinsert" -i $1
 
-toinsert=`cat $1 | grep '<p>' | $sedcom "/H[.]E[.]/d;/2020\s*$/d" | $sedcom -n 1p | $sedcom 's/<p>//' | $sedcom 's/<[^<>]+>//g' | $sedcom 's|^|/<title/i\\\\<meta property="description" content="|' | $sedcom 's|$|">|'`
+toinsert=`cat $1 | grep '<p>' | $sedcom '/H[.]E[.]/d;/20(19|20)\s*<[^<>]+>\s*$/d' | $sedcom -n 1p | $sedcom 's/<p>//' | $sedcom 's/<[^<>]+>//g' | $sedcom 's|^|/<title/i\\\\<meta property="description" content="|' | $sedcom 's|$|">|'`
 $sedcom "$toinsert" -i $1
