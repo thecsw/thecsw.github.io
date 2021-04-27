e7e7f47b7e6b3f0e5eed3e500fda432d884b5beb
Sandy Urazayev <ctu@ku.edu>
2021-04-27 17:58:49 -0500

    Remove tags from descriptions. (Actually works)
    Signed-off-by: Sandy Urazayev <ctu@ku.edu>

--- sh/og_desc.sh
+++ sh/og_desc.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-sedcom=sed
+# Use GNU sed if on Darwin
+sedcom="sed -E"
+arch=`uname -s`
+if [ $arch = "Darwin" ]
+then
+    sedcom="gsed -E"
+fi
 
-toinsert=`cat $1 | grep '<p>' | $sedcom "/H[.]E[.]/d" | $sedcom -n 1p | $sedcom "s/<p>//" | $sedcom 's/<[^<>]+>//g' | $sedcom 's|^|/<title/i\\\\<meta property="og:description" content="|' | $sedcom 's|$|">|'`
+toinsert=`cat $1 | grep '<p>' | $sedcom "/H[.]E[.]/d;/2020\s*$/d" | $sedcom -n 1p | $sedcom 's/<p>//' | $sedcom 's/<[^<>]+>//g' | $sedcom 's|^|/<title/i\\\\<meta property="og:description" content="|' | $sedcom 's|$|">|'`
 $sedcom "$toinsert" -i $1
 
-toinsert=`cat $1 | grep '<p>' | $sedcom "/H[.]E[.]/d" | $sedcom -n 1p | $sedcom "s/<p>//" | $sedcom 's/<[^<>]+>//g' | $sedcom 's|^|/<title/i\\\\<meta property="description" content="|' | $sedcom 's|$|">|'`
+toinsert=`cat $1 | grep '<p>' | $sedcom "/H[.]E[.]/d;/2020\s*$/d" | $sedcom -n 1p | $sedcom 's/<p>//' | $sedcom 's/<[^<>]+>//g' | $sedcom 's|^|/<title/i\\\\<meta property="description" content="|' | $sedcom 's|$|">|'`
 $sedcom "$toinsert" -i $1
