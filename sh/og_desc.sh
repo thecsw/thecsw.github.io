e828e79284b2b40ede69947a8e8ac8634a283884
Sandy Urazayev <ctu@ku.edu>
2021-04-27 18:09:23 -0500

    Better description for the description script
    Signed-off-by: Sandy Urazayev <ctu@ku.edu>

--- sh/og_desc.sh
+++ sh/og_desc.sh
@@ -8,8 +8,16 @@ then
     sedcom="gsed -E"
 fi
 
-toinsert=`cat $1 | grep '<p>' | $sedcom '/H[.]E[.]/d;/20(19|20)\s*<[^<>]+>\s*$/d' | $sedcom -n 1p | $sedcom 's/<p>//' | $sedcom 's/<[^<>]+>//g' | $sedcom 's|^|/<title/i\\\\<meta property="og:description" content="|' | $sedcom 's|$|">|'`
+# Here is what happens
+# 1. We find all the paragraphs in the final html file
+# 2. We remove lines that are just timestamps of the webpage
+# 3. We grab the very first line after filtering is done in 2.
+# 4. Remove the HTML tags in the description
+# 5. Preppend the meta property
+# 6. Append the closing tags
+
+toinsert=`cat $1 | grep '<p>' | $sedcom '/H[.]E[.]/d;/20(19|20)\s*<[^<>]+>\s*$/d' | $sedcom -n 1p | $sedcom 's/<[^<>]+>//g' | $sedcom 's|^|/<title/i\\\\<meta property="og:description" content="|' | $sedcom 's|$|">|'`
 $sedcom "$toinsert" -i $1
 
-toinsert=`cat $1 | grep '<p>' | $sedcom '/H[.]E[.]/d;/20(19|20)\s*<[^<>]+>\s*$/d' | $sedcom -n 1p | $sedcom 's/<p>//' | $sedcom 's/<[^<>]+>//g' | $sedcom 's|^|/<title/i\\\\<meta property="description" content="|' | $sedcom 's|$|">|'`
+toinsert=`cat $1 | grep '<p>' | $sedcom '/H[.]E[.]/d;/20(19|20)\s*<[^<>]+>\s*$/d' | $sedcom -n 1p | $sedcom 's/<[^<>]+>//g' | $sedcom 's|^|/<title/i\\\\<meta property="description" content="|' | $sedcom 's|$|">|'`
 $sedcom "$toinsert" -i $1
