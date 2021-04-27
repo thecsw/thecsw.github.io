d30c401eb9c575a5dd0733612fc27bb492bb33c7
thecsw <ctu@ku.edu>
2021-04-27 22:19:03 +0000

    Deploying to master from source @ 1d36bba5b4b0f2df0ae7763db1e07a6645879e3b 🚀

--- sh/og_desc.sh
+++ sh/og_desc.sh
@@ -2,5 +2,5 @@
 
 sedcom=sed
 
-toinsert=`cat $1 | grep '<p>' | $sedcom -n 2p | $sedcom "s/<p>//" | $sedcom 's|^|/<title/i\\\\<meta property="og:description" content="|' | $sedcom 's|$|">|'`
+toinsert=`cat $1 | grep '<p>' | $sedcom "/H[.]E[.]/d" | $sedcom -n 1p | $sedcom "s/<p>//" | $sedcom 's|^|/<title/i\\\\<meta property="og:description" content="|' | $sedcom 's|$|">|'`
 $sedcom "$toinsert" -i $1
