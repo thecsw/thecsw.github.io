8ad08925e49a66be9c5d389b778af7d2bd509438
Sandy Urazayev <ctu@ku.edu>
2021-04-27 17:24:35 -0500

    Remove tags from meta descriptions
    Signed-off-by: Sandy Urazayev <ctu@ku.edu>

--- sh/og_desc.sh
+++ sh/og_desc.sh
@@ -5,5 +5,5 @@ sedcom=sed
 toinsert=`cat $1 | grep '<p>' | $sedcom "/H[.]E[.]/d" | $sedcom -n 1p | $sedcom "s/<p>//" | $sedcom 's/<[^<>]+>//g' | $sedcom 's|^|/<title/i\\\\<meta property="og:description" content="|' | $sedcom 's|$|">|'`
 $sedcom "$toinsert" -i $1
 
-toinsert=`cat $1 | grep '<p>' | $sedcom "/H[.]E[.]/d" | $sedcom -n 1p | $sedcom "s/<p>//" | $sedcom 's|^|/<title/i\\\\<meta property="description" content="|' | $sedcom 's|$|">|'`
+toinsert=`cat $1 | grep '<p>' | $sedcom "/H[.]E[.]/d" | $sedcom -n 1p | $sedcom "s/<p>//" | $sedcom 's/<[^<>]+>//g' | $sedcom 's|^|/<title/i\\\\<meta property="description" content="|' | $sedcom 's|$|">|'`
 $sedcom "$toinsert" -i $1
