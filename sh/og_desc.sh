c10f9d66a303ec5317889e928f18466f2a88f944
Sandy Urazayev <ctu@ku.edu>
2021-12-28 20:25:21 -0600

    Formatted `og_desc.sh`
    Signed-off-by: Sandy Urazayev <ctu@ku.edu>

--- sh/og_desc.sh
+++ sh/og_desc.sh
@@ -18,17 +18,17 @@ fi
 toinsert=$(cat $1 |
 	grep '<p>' |
 	$sedcom '/H[.]E[.]/d;/20(19|20|21|22|23|24)\s*<[^<>]+>\s*$/d' |
-        $sedcom -n 1p |
-        $sedcom 's/<[^<>]+>//g' |
-        $sedcom 's|^|/<title/i\\\\<meta property="og:description" content="|' |
-        $sedcom 's|$|...">|')
+	$sedcom -n 1p |
+	$sedcom 's/<[^<>]+>//g' |
+	$sedcom 's|^|/<title/i\\\\<meta property="og:description" content="|' |
+	$sedcom 's|$|...">|')
 $sedcom "$toinsert" -i $1
 
 toinsert=$(cat $1 |
-               grep '<p>' |
-               $sedcom '/H[.]E[.]/d;/20(19|20|21|22|23|24)\s*<[^<>]+>\s*$/d' |
-               $sedcom -n 1p |
-               $sedcom 's/<[^<>]+>//g' |
-               $sedcom 's|^|/<title/i\\\\<meta property="description" content="|' |
-               $sedcom 's|$|...">|')
+	grep '<p>' |
+	$sedcom '/H[.]E[.]/d;/20(19|20|21|22|23|24)\s*<[^<>]+>\s*$/d' |
+	$sedcom -n 1p |
+	$sedcom 's/<[^<>]+>//g' |
+	$sedcom 's|^|/<title/i\\\\<meta property="description" content="|' |
+	$sedcom 's|$|...">|')
 $sedcom "$toinsert" -i $1
