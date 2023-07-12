dc9cb2ca1dfcab0fa771543944c245a34139c3e0
Sandy <sandy@sandyuraz.com>
2023-07-12 00:13:37 -0500

    Added kaguya
    Signed-off-by: Sandy <sandy@sandyuraz.com>

---
+++ anime/kaguya/love.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', function() { 
+      let title = document.querySelector(".section-1");
+      title.innerHTML = title.innerHTML.replace(/Love/, '<ss style="color:#ff001f">Love</ss>');
+  }, false);
