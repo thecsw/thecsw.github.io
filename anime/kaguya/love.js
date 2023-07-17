22f5d913b7bc02fe2fd750eea7dea3e8f5ad3abd
Sandy <sandy@sandyuraz.com>
2023-07-16 22:44:52 -0400

    Have scripts anchored to the document load, not window
    Signed-off-by: Sandy <sandy@sandyuraz.com>

--- anime/kaguya/love.js
+++ anime/kaguya/love.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function() { 
+document.addEventListener('DOMContentLoaded', function() { 
       let title = document.querySelector(".section-1");
       title.innerHTML = title.innerHTML.replace(/Love/, '<ss style="color:#ff001f">Love</ss>');
   }, false);
