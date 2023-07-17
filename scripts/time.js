5f93632590035839b0816954441473131f2bd399
thecsw <16922860+thecsw@users.noreply.github.com>
2023-07-17 02:53:02 +0000

    Deploying to main from source @ 1b515a75c60914e07e2bfdb11d6df1a4d0dc1aa0 🚀

--- scripts/time.js
+++ scripts/time.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() { 
+window.addEventListener('load', function() { 
   let time = document.querySelector("#hetime");
   const day = 24 * 60 * 60 * 1000;
   cityDuringDayEmojis = ["🌃", "🌃", "🌌", "🌌", "🌉", "🌉",
