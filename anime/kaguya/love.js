e46916109815c4ca5a782569f32ffb8a575cacf9
Sandy <sandy@sandyuraz.com>
2023-07-16 23:43:40 -0400

    format kaguya scripts
    Signed-off-by: Sandy <sandy@sandyuraz.com>

--- anime/kaguya/love.js
+++ anime/kaguya/love.js
@@ -1,4 +1,11 @@
-window.addEventListener('load', function() { 
-      let title = document.querySelector(".section-1");
-      title.innerHTML = title.innerHTML.replace(/Love/, '<ss style="color:#ff001f">Love</ss>');
-  }, false);
+window.addEventListener(
+        "load",
+        function () {
+                let title = document.querySelector(".section-1");
+                title.innerHTML = title.innerHTML.replace(
+                        /Love/,
+                        '<ss style="color:#ff001f">Love</ss>'
+                );
+        },
+        false
+);
