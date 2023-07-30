76c4173110b1e43c9495a7965ea0abeeb9ec3db5
thecsw <16922860+thecsw@users.noreply.github.com>
2023-07-30 23:24:47 +0000

    Deploying to main from source @ 845366908ff4fc1d7b091b293286baa4a22d2d58 🚀

--- scripts/rss.sh
+++ scripts/rss.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-bin="./darkness.com"
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    bin="darkness"
-fi
-
-${bin} misa -rss feed.xml -rss-dirs ./,anime,blogs,books,plastic,shokugeki,stories,writings
+./darkness.com misa -rss feed.xml -rss-dirs ./,anime,blogs,books,plastic,shokugeki,stories,writings
