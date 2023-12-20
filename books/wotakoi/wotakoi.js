02dc265007ef9aef17c7555d1b3db297837922bc
thecsw <16922860+thecsw@users.noreply.github.com>
2023-12-20 06:52:52 +0000

    Deploying to main from @ thecsw/thecsw.github.io@99c7dc4d6ae7274b1517b578fc863d68817f8db1 🚀

---
+++ books/wotakoi/wotakoi.js
@@ -0,0 +1,8 @@
+window.addEventListener(
+        "load",
+        function () {
+            let title = document.querySelector(".section-1");
+            title.innerHTML="<span>W</span><span>O</span><span>T</span><span>A</span><span>K</span><span>O</span><span>I</span>";
+        },
+        false
+);
